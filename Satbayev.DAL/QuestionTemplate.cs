fb7fca22ed1f7e255ccf705f81613c0118074a32
Khan-Slava <vaceslavhan22@gmail.com>
2023-10-21 15:47:17 +0600

    Добавьте файлы проекта.

---
+++ Satbayev.DAL/QuestionTemplate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Satbayev.DAL
+{
+    public class QuestionTemplate
+    {
+        public int Id { get; set; }
+        public DateTime CreateDate { get; set; }
+        public string Author { get; set; }
+        public string Questions { get; set; }
+        public double Mark { get; set; }
+        public string Level { get; set; }
+        public string ChangeAuthor { get; set; }
+        public DateTime ChangeData { get; set; }
+        public Category Category { get; set; }
+        public int CategoryID { get; set; }
+
+    }
+
+}
---
+++ Satbayev.DAL/Repository.cs
@@ -0,0 +1,35 @@
+﻿using LiteDB;
+namespace Satbayev.DAL
+{
+    public class Repository
+    {
+        public bool createQuestion(QuestionTemplate question) 
+        {
+            
+            try
+            {
+                using (var db = new LiteDatabase(""))
+                {
+                
+                    var col = db.GetCollection<QuestionTemplate>("QuestionTemplate");
+                    col.Insert(question);
+                    return true;
+
+                } 
+
+            }
+            catch (Exception ex) {
+                return false;
+            }
+
+        }
+        
+
+        public string GetAllQuestions() 
+        {
+
+            return null;
+        }
+    }
+
+}
